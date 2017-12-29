116550991be48ec1a7814b996ecf49ea7603ddb7
Chris Sullivan <email-blocked>
2017-12-29 19:11:05 +0000

    Update bitmap editor to seperate out command and parameters

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -10,13 +10,16 @@ class BitmapEditor
     return puts 'please provide correct file' if file.nil? || !File.exist?(file)
 
     File.open(file).each do |line|
-      line = line.chomp
-      case line
+      line = line.chomp.split
+      command = line[0]
+      parameters = line[1..-1]
+
+      case command
       when 'S'
-        command = Show.new
+        command = Show.new(*parameters)
         command.execute
       else
-        command = Unknown.new
+        command = Unknown.new(*parameters)
         command.execute
       end
     end
