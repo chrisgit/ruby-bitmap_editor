ed1bad4657391965a6bbc3e5c2d05edf843206ea
Chris Sullivan <email-blocked>
2017-12-30 17:05:25 +0000

    Bitmap colour pixel, parameter range checks

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -10,4 +10,9 @@ class Bitmap
   def dump
     @image.each_slice(@columns).map(&:join)
   end
+
+  def colour_pixel(column, row, colour)
+    raise RangeError, 'Bitmap: Column is out of range' if column < 1 || column > @columns
+    raise RangeError, 'Bitmap: Row is out of range' if row < 1 || row > @rows
+  end
 end
--- spec/unit/lib/bitmap_spec.rb
+++ spec/unit/lib/bitmap_spec.rb
@@ -18,4 +18,40 @@ describe 'bitmap' do
       expect(bitmap.dump).to eq(Array.new(6, 'OOOOO'))
     end
   end
+  describe '#colour_pixel' do
+    let(:bitmap) { Bitmap.new(5, 6) }
+    context 'column' do
+      context 'less than 1' do
+        it 'throws RangeError' do
+          expect { bitmap.colour_pixel(0, 3, 'C') }.to raise_error(RangeError)
+        end
+      end
+
+      context 'greater than bitmap columns' do
+        it 'throws RangeError' do
+          expect { bitmap.colour_pixel(8, 3, 'C') }.to raise_error(RangeError)
+        end
+      end
+    end
+
+    context 'row' do
+      context 'less than 1' do
+        it 'throws RangeError' do
+          expect { bitmap.colour_pixel(3, 0, 'C') }.to raise_error(RangeError)
+        end
+      end
+
+      context 'greater than bitmap rows' do
+        it 'throws RangeError' do
+          expect { bitmap.colour_pixel(3, 8, 'C') }.to raise_error(RangeError)
+        end
+      end
+    end
+
+    context 'columns and rows within bounds' do
+      it 'does not throw an error' do
+        expect { bitmap.colour_pixel(3, 3, 'C') }.not_to raise_error
+      end
+    end
+  end
 end
