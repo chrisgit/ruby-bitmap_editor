89e7dedb9ba8e5383d1650f6e57fb7272e763f44
Chris Sullivan <email-blocked>
2017-12-30 17:09:34 +0000

    Bitmap colour pixel added validation for colour

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -14,5 +14,13 @@ class Bitmap
   def colour_pixel(column, row, colour)
     raise RangeError, 'Bitmap: Column is out of range' if column < 1 || column > @columns
     raise RangeError, 'Bitmap: Row is out of range' if row < 1 || row > @rows
+    raise ArgumentError, 'Colour: The specified colour is not valid' unless valid_colour?(colour)
+  end
+
+  private
+
+  def valid_colour?(colour)
+    return false if colour.length != 1
+    ('A'..'Z').cover? colour
   end
 end
--- spec/unit/lib/bitmap_spec.rb
+++ spec/unit/lib/bitmap_spec.rb
@@ -20,37 +20,51 @@ describe 'bitmap' do
   end
   describe '#colour_pixel' do
     let(:bitmap) { Bitmap.new(5, 6) }
-    context 'column' do
-      context 'less than 1' do
-        it 'throws RangeError' do
-          expect { bitmap.colour_pixel(0, 3, 'C') }.to raise_error(RangeError)
+    context 'parameters' do
+      context 'column' do
+        context 'less than 1' do
+          it 'throws RangeError' do
+            expect { bitmap.colour_pixel(0, 3, 'C') }.to raise_error(RangeError)
+          end
+        end
+
+        context 'greater than bitmap columns' do
+          it 'throws RangeError' do
+            expect { bitmap.colour_pixel(8, 3, 'C') }.to raise_error(RangeError)
+          end
         end
       end
 
-      context 'greater than bitmap columns' do
-        it 'throws RangeError' do
-          expect { bitmap.colour_pixel(8, 3, 'C') }.to raise_error(RangeError)
+      context 'row' do
+        context 'less than 1' do
+          it 'throws RangeError' do
+            expect { bitmap.colour_pixel(3, 0, 'C') }.to raise_error(RangeError)
+          end
+        end
+
+        context 'greater than bitmap rows' do
+          it 'throws RangeError' do
+            expect { bitmap.colour_pixel(3, 8, 'C') }.to raise_error(RangeError)
+          end
         end
       end
-    end
 
-    context 'row' do
-      context 'less than 1' do
-        it 'throws RangeError' do
-          expect { bitmap.colour_pixel(3, 0, 'C') }.to raise_error(RangeError)
+      context 'columns and rows within bounds' do
+        it 'does not throw an error' do
+          expect { bitmap.colour_pixel(3, 3, 'C') }.not_to raise_error
         end
       end
-
-      context 'greater than bitmap rows' do
-        it 'throws RangeError' do
-          expect { bitmap.colour_pixel(3, 8, 'C') }.to raise_error(RangeError)
+      context 'colour' do
+        context 'not a single character' do
+          it 'throws ArgumentError' do
+            expect { bitmap.colour_pixel(3, 3, 'Blue') }.to raise_error(ArgumentError)
+          end
         end
-      end
-    end
-
-    context 'columns and rows within bounds' do
-      it 'does not throw an error' do
-        expect { bitmap.colour_pixel(3, 3, 'C') }.not_to raise_error
+        context 'not upper case' do
+          it 'throws ArgumentError' do
+            expect { bitmap.colour_pixel(3, 3, 'b') }.to raise_error(ArgumentError)
+          end
+        end
       end
     end
   end
