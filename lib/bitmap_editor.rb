792c4345853739f5771cbbbf4187ef4d7ba0a801
Chris Sullivan <email-blocked>
2017-12-30 19:26:04 +0000

    Refactor bitmap editor to use a lookup for commands

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -6,42 +6,39 @@ require_relative 'commands/colour'
 require_relative 'commands/clear'
 require_relative 'commands/hline'
 require_relative 'commands/vline'
-
 ##
 # Bitmap editor, read editing instructions from file
 class BitmapEditor
+  def initialize()
+    load_editor_commands
+  end
+
   def run(file)
     return puts 'please provide correct file' if file.nil? || !File.exist?(file)
     bitmap = nil
 
     File.open(file).each do |line|
       line = line.chomp.split
-      command = line[0]
+      edit_command = line[0]
       parameters = line[1..-1]
-
-      case command
-      when 'S'
-        command = Show.new(*parameters)
-        command.execute(bitmap)
-      when 'I'
-        command = Create.new(*parameters)
-        bitmap = command.execute(bitmap)
-      when 'L'
-        command = Colour.new(*parameters)
-        command.execute(bitmap)
-      when 'C'
-        command = Clear.new(*parameters)
-        command.execute(bitmap)
-      when 'H'
-        command = HorizontalLine.new(*parameters)
-        command.execute(bitmap)
-      when 'V'
-        command = VerticalLine.new(*parameters)
-        command.execute(bitmap)
-      else
-        command = Unknown.new(*parameters)
-        command.execute(bitmap)
-      end
+      klass = @commands.fetch(edit_command, Unknown)
+      command = klass.new(*parameters)
+      updated_bitmap = command.execute(bitmap)
+      bitmap = updated_bitmap if updated_bitmap.is_a?(Bitmap)
     end
+  end
+
+  private
+
+  def load_editor_commands
+    @commands = {
+      'S' => Show,
+      'I' => Create,
+      'C' => Clear,
+      'L' => Colour,
+      'H' => HorizontalLine,
+      'V' => VerticalLine
+    }
+    @commands.default = Unknown
   end
 end
