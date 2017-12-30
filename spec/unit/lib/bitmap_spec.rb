323f62980e826a6ca34234c332ca9b0395a003ae
Chris Sullivan <email-blocked>
2017-12-30 17:02:18 +0000

    Move repeated bitmap create code into memoized variable

--- spec/unit/lib/bitmap_spec.rb
+++ spec/unit/lib/bitmap_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe 'bitmap' do
+  let (:bitmap) { Bitmap.new(5, 6) }
   describe '#initialize' do
     it 'creates new bitmap of size M N' do
-      bitmap = Bitmap.new(5, 6)
       expect(bitmap.columns).to eq(5)
       expect(bitmap.rows).to eq(6)
+    end
+
+    it 'colour is white' do
+      expect(bitmap.dump).to eq(Array.new(6, 'OOOOO'))
     end
   end
 
   describe '#dump' do
     it 'returns array of bitmap rows' do
-      bitmap = Bitmap.new(5, 6)
       expect(bitmap.dump).to eq(Array.new(6, 'OOOOO'))
     end
   end
