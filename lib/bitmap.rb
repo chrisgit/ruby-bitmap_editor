725d927bcda0bd0368d0e1efc16f3b44553ecfa2
Chris Sullivan <email-blocked>
2018-01-02 21:10:23 +0000

    Design change, move draw horizontal / vertical to bitmap

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -12,11 +12,30 @@ class Bitmap
   end
 
   def colour_pixel(column, row, colour)
-    raise RangeError, 'Bitmap: Column is out of range' if column < 1 || column > @columns
-    raise RangeError, 'Bitmap: Row is out of range' if row < 1 || row > @rows
+    column_range_check(column, column)
+    row_range_check(row, row)
     raise ArgumentError, 'Colour: The specified colour is not valid' unless valid_colour?(colour)
-    row_offset = (row - 1) * @columns
-    @image[row_offset + column - 1] = colour
+    set_pixel_colour(column, row, colour)
+  end
+
+  def draw_horizontal(start_column, end_column, row, colour)
+    start_column, end_column = end_column, start_column if start_column > end_column
+    column_range_check(start_column, end_column)
+    row_range_check(row, row)
+
+    start_column.upto(end_column) do |column|
+      set_pixel_colour(column, row, colour)
+    end
+  end
+
+  def draw_vertical(column, start_row, end_row, colour)
+    start_row, end_row = end_row, start_row if start_row > end_row
+    column_range_check(column, column)
+    row_range_check(start_row, end_row)
+
+    start_row.upto(end_row) do |row|
+      set_pixel_colour(column, row, colour)
+    end
   end
 
   def dump
@@ -29,4 +48,19 @@ class Bitmap
     return false if colour.length != 1
     ('A'..'Z').cover? colour
   end
+
+  def column_range_check(start_column, end_column)
+    return if start_column > 0 && end_column <= @columns
+    raise RangeError, 'Bitmap: Column is out of range'
+  end
+
+  def row_range_check(start_row, end_row)
+    return if start_row > 0 && end_row <= @rows
+    raise RangeError, 'Bitmap: Row is out of range'
+  end
+
+  def set_pixel_colour(column, row, colour)
+    row_offset = (row - 1) * @columns
+    @image[row_offset + column - 1] = colour
+  end    
 end
--- spec/unit/lib/bitmap_spec.rb
+++ spec/unit/lib/bitmap_spec.rb
@@ -18,6 +18,7 @@ describe Bitmap do
       expect(bitmap.dump).to eq(Array.new(6, 'OOOOO'))
     end
   end
+
   describe '#colour_pixel' do
     let(:bitmap) { Bitmap.new(5, 6) }
     context 'parameters' do
@@ -81,12 +82,86 @@ describe Bitmap do
       end
     end
   end
+
   describe '#clear' do
     it 'resets bitmap colours to default' do
       bitmap = BitmapTestHelper.create_and_fill(5, 6, '!')
       bitmap.clear()
 
       expect(bitmap.dump).to eq(Array.new(6, 'OOOOO'))
+    end
+  end
+
+  describe '#draw_horizontal' do
+    context 'parameters' do
+      context 'start column' do
+        context 'less than 1' do
+          it 'throws RangeError' do
+            expect { bitmap.draw_horizontal(0, 3, 4, 'O') }.to raise_error(RangeError)
+          end
+        end
+        context 'greater than bitmap columns' do
+          it 'throws RangeError' do
+            expect { bitmap.draw_horizontal(11, 3, 4, 'O') }.to raise_error(RangeError)
+          end
+        end
+      end
+      context 'end column' do
+        context 'less than 1' do
+          it 'throws RangeError' do
+            expect { bitmap.draw_horizontal(3, 0, 4, 'O') }.to raise_error(RangeError)
+          end
+        end
+        context 'greater than bitmap columns' do
+          it 'throws RangeError' do
+            expect { bitmap.draw_horizontal(2, 11, 4, 'O') }.to raise_error(RangeError)
+          end
+        end
+      end
+      context 'valid' do
+        it 'draws horizontal line' do
+          bitmap.draw_horizontal(1, 5, 3, 'Z')
+          output = ['OOOOO', 'OOOOO', 'ZZZZZ', 'OOOOO', 'OOOOO', 'OOOOO']
+
+          expect(bitmap.dump).to eq(output)
+        end
+      end
+    end
+  end
+
+  describe '#draw_vertical' do
+    context 'start row' do
+      context 'less than 1' do
+        it 'throws RangeError' do
+          expect { bitmap.draw_vertical(3, 0, 5, 'O') }.to raise_error(RangeError)
+        end
+      end
+      context 'greater than bitmap rows' do
+        it 'throws RangeError' do
+          expect { bitmap.draw_vertical(3, 7, 5, 'O') }.to raise_error(RangeError)
+        end
+      end
+    end
+    context 'end row' do
+      context 'less than 1' do
+        it 'throws RangeError' do
+          expect { bitmap.draw_vertical(3, 2, 0, 'O') }.to raise_error(RangeError)
+        end
+      end
+      context 'greater than bitmap rows' do
+        it 'throws RangeError' do
+          expect { bitmap.draw_vertical(3, 2, 7, 'O') }.to raise_error(RangeError)
+        end
+      end
+    end
+    context 'valid' do
+      let(:bitmap) { Bitmap.new(5, 6) }
+      it 'draws vertical line' do
+        bitmap.draw_vertical(4, 5, 3, 'Z')
+        output = ['OOOOO', 'OOOOO', 'OOOZO', 'OOOZO', 'OOOZO', 'OOOOO']
+
+        expect(bitmap.dump).to eq(output)
+      end
     end
   end
 end
