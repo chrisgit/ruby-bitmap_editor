580e3dcb0947def76542e47cab56046907406956
Chris Sullivan <email-blocked>
2018-01-02 21:17:49 +0000

    Design change, move draw horizontal / vertical to bitmap, Vertical command

--- lib/commands/vline.rb
+++ lib/commands/vline.rb
@@ -7,26 +7,10 @@ class VerticalLine
     @start_row = start_row.to_i
     @end_row = end_row.to_i
     @colour = colour.to_s
-    @start_row, @end_row = @end_row, @start_row if @start_row > @end_row
   end
 
   def execute(bitmap = NilBitmap.new)
     bitmap ||= NilBitmap.new
-    raise ArgumentError, 'VerticalLine: Bitmap has not been created' if bitmap.is_a? NilBitmap
-    range_check(bitmap.rows)
-    draw_vertical(bitmap)
-  end
-
-  private
-
-  def range_check(bitmap_rows)
-    raise RangeError, 'Vertical Line: Start Row is out of range' if @start_row < 1 || @start_row > bitmap_rows
-    raise RangeError, 'Vertical Line: End Row is out of range' if @end_row < 1 || @end_row > bitmap_rows
-  end
-
-  def draw_vertical(bitmap)
-    @start_row.upto(@end_row) do |row|
-      bitmap.colour_pixel(@column, row, @colour)
-    end
+    bitmap.draw_vertical(@column, @start_row, @end_row, @colour)
   end
 end
--- spec/unit/lib/commands/vline_spec.rb
+++ spec/unit/lib/commands/vline_spec.rb
@@ -5,29 +5,17 @@ describe VerticalLine do
     context 'parameters' do
       context 'not enough passed' do
         it 'throws ArgumentError' do
-          parameters = 'V 1'.split[1..-1]
-          expect { VerticalLine.new(*parameters) }.to raise_error(ArgumentError)
+          expect { VerticalLine.new(*['1']) }.to raise_error(ArgumentError)
         end
       end
       context 'too many passed' do
         it 'throws ArgumentError' do
-          parameters = 'V 1 3 4 Z Hello'.split[1..-1]
-          expect { VerticalLine.new(*parameters) }.to raise_error(ArgumentError)
+          expect { VerticalLine.new(*['1', '3', '4', 'Z', 'Hello']) }.to raise_error(ArgumentError)
         end
       end
       context 'exact number passed' do
         it 'returns object type of VerticalLine' do
-          parameters = 'V 1 5 4 Z'.split[1..-1]
-          expect(VerticalLine.new(*parameters)).to be_a(VerticalLine)
-        end
-      end
-      context 'start row greater than end row' do
-        it 'transposes the values' do
-          parameters = 'V 5 7 3 Z'.split[1..-1]
-          vl = VerticalLine.new(*parameters)
-
-          expect(vl.start_row).to be(3)
-          expect(vl.end_row).to be(7)
+          expect(VerticalLine.new(*['1', '5', '4', 'Z'])).to be_a(VerticalLine)
         end
       end
     end
@@ -36,55 +24,18 @@ describe VerticalLine do
   describe '#execute' do
     context 'bitmap is null' do
       it 'throws ArgumentError' do
-        parameters = 'V 1 5 6 Z'.split[1..-1]
-        command = VerticalLine.new(*parameters)
+        command = VerticalLine.new(*['1', '5', '6', 'Z'])
         expect { command.execute(nil) }.to raise_error(ArgumentError)
       end
     end
     context 'bitmap is not null' do
       let(:bitmap) { Bitmap.new(5, 6) }
-      context 'start row' do
-        context 'less than 1' do
-          it 'throws RangeError' do
-            parameters = 'V 3 0 5 Z'.split[1..-1]
-            command = VerticalLine.new(*parameters)
-            expect { command.execute(bitmap) }.to raise_error(RangeError)
-          end
-        end
-        context 'greater than bitmap rows' do
-          it 'throws RangeError' do
-            parameters = 'V 3 7 5 Z'.split[1..-1]
-            command = VerticalLine.new(*parameters)
-            expect { command.execute(bitmap) }.to raise_error(RangeError)
-          end
-        end
-      end
-      context 'end row' do
-        context 'less than 1' do
-          it 'throws RangeError' do
-            parameters = 'V 3 2 0 Z'.split[1..-1]
-            command = VerticalLine.new(*parameters)
-            expect { command.execute(bitmap) }.to raise_error(RangeError)
-          end
-        end
-        context 'greater than bitmap rows' do
-          it 'throws RangeError' do
-            parameters = 'V 3 2 7 Z'.split[1..-1]
-            command = VerticalLine.new(*parameters)
-            expect { command.execute(bitmap) }.to raise_error(RangeError)
-          end
-        end
-      end
-      context 'valid' do
-        let(:bitmap) { Bitmap.new(5, 6) }
-        it 'draws vertical line' do
-          parameters = 'V 4 5 3 Z'.split[1..-1]
-          command = VerticalLine.new(*parameters)
-          command.execute(bitmap)
-          output = ['OOOOO', 'OOOOO', 'OOOZO', 'OOOZO', 'OOOZO', 'OOOOO']
+      it 'draws vertical line' do
+        command = VerticalLine.new(*['4', '5', '3', 'Z'])
+        command.execute(bitmap)
+        output = ['OOOOO', 'OOOOO', 'OOOZO', 'OOOZO', 'OOOZO', 'OOOOO']
 
-          expect(bitmap.dump).to eq(output)
-        end
+        expect(bitmap.dump).to eq(output)
       end
     end
   end
