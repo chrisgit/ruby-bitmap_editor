13ade7d21bdcb26ad12c44c51c78bfa090ce0f64
Chris Sullivan <email-blocked>
2018-01-03 23:11:53 +0000

    Use bitmap draw method in colour pixel command

--- lib/commands/colour.rb
+++ lib/commands/colour.rb
@@ -1,14 +1,15 @@
 ##
 # Show the contents of the bitmap
 class Colour
+  attr_reader :range, :colour
+
   def initialize(column, row, colour)
-    @column = column.to_i
-    @row = row.to_i
     @colour = colour.to_s
+    @range = BitmapRange.new(column.to_i, row.to_i, column.to_i, row.to_i)
   end
 
   def execute(bitmap = NilBitmap.new)
     bitmap ||= NilBitmap.new
-    bitmap.colour_pixel(@column, @row, @colour)
+    bitmap.draw(@range, @colour)
   end
 end
