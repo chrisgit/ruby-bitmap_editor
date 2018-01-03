cfa8289f57e7e0474a3c1957e5510d8ab181f2fd
Chris Sullivan <email-blocked>
2018-01-03 23:13:39 +0000

    Use bitmap draw method in horizontal line command

--- lib/commands/hline.rb
+++ lib/commands/hline.rb
@@ -1,17 +1,15 @@
 ##
 # Draw a horizontal line in the bitmap
 class HorizontalLine
-  attr_reader :start_column, :end_column, :row, :colour
+  attr_reader :range, :colour
 
   def initialize(start_column, end_column, row, colour)
-    @start_column = start_column.to_i
-    @end_column = end_column.to_i
-    @row = row.to_i
     @colour = colour.to_s
+    @range = BitmapRange.new(start_column.to_i, row.to_i, end_column.to_i, row.to_i)
   end
 
   def execute(bitmap = NilBitmap.new)
     bitmap ||= NilBitmap.new
-    bitmap.draw_horizontal(@start_column, @end_column, @row, @colour)
+    bitmap.draw(@range, @colour)
   end
 end
