6aa20348ef5fb2e179df2ee7410210d597da4705
Chris Sullivan <email-blocked>
2018-01-04 18:32:03 +0000

    Change reference instance from nilbitmap to bitmap

--- spec/unit/lib/nilbitmap_spec.rb
+++ spec/unit/lib/nilbitmap_spec.rb
@@ -1,35 +1,35 @@
 require 'spec_helper'
 
 describe NilBitmap do
-  let (:nilbitmap) { NilBitmap.new }
+  let (:bitmap) { NilBitmap.new }
   describe '#initialize' do
     it 'creates new bitmap of size 0 0' do
-      expect(nilbitmap.columns).to eq(0)
-      expect(nilbitmap.rows).to eq(0)
+      expect(bitmap.columns).to eq(0)
+      expect(bitmap.rows).to eq(0)
     end
   end
 
   describe '#dump' do
     it 'returns "There is no image"' do
-      expect(nilbitmap.dump).to eq('There is no image')
+      expect(bitmap.dump).to eq('There is no image')
     end
   end
 
   describe '#colour_pixel' do
     it 'throws ArgumentError"' do
-      expect { nilbitmap.colour_pixel(1, 1, 'O') }.to raise_error(ArgumentError)
+      expect { bitmap.colour_pixel(1, 1, 'O') }.to raise_error(ArgumentError)
     end
   end
 
   describe '#clear' do
     it 'throws ArgumentError"' do
-      expect { nilbitmap.clear }.to raise_error(ArgumentError)
+      expect { bitmap.clear }.to raise_error(ArgumentError)
     end
   end
 
   describe '#draw' do
     it 'throws ArgumentError"' do
-      expect { nilbitmap.draw(Range.new(1,1,5,5), 'O') }.to raise_error(ArgumentError)
+      expect { bitmap.draw(Range.new(1,1,5,5), 'O') }.to raise_error(ArgumentError)
     end
   end
 end
