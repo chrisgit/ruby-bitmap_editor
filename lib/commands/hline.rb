de9fbe5671106770a82f66482511232b1c12ee16
Chris Sullivan <email-blocked>
2018-01-04 22:34:41 +0000

    Tidy code, single call to cast / convert

--- lib/commands/hline.rb
+++ lib/commands/hline.rb
@@ -5,7 +5,10 @@ class HorizontalLine
 
   def initialize(start_column, end_column, row, colour)
     @colour = colour.to_s
-    @range = BitmapRange.new(start_column.to_i, row.to_i, end_column.to_i, row.to_i)
+    start_column = start_column.to_i
+    end_column = end_column.to_i
+    row = row.to_i
+    @range = BitmapRange.new(start_column, row, end_column, row)
   end
 
   def execute(bitmap = NilBitmap.new)
