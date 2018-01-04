22ad2270ea48008c87d9acfc2387c04ae5f04fd0
Chris Sullivan <email-blocked>
2018-01-04 20:32:19 +0000

    Refactored tests to make them easier to scan / readable

--- spec/shared/bitmap.rb
+++ spec/shared/bitmap.rb
@@ -2,7 +2,7 @@
 # Classes that should behave like a bitmap
 shared_examples 'bitmap' do
   it 'responds to clear' do
-      expect(bitmap).to respond_to(:clear)
+    expect(bitmap).to respond_to(:clear)
   end
 
   it 'responds to draw' do
@@ -13,4 +13,19 @@ shared_examples 'bitmap' do
     expect(bitmap).to respond_to(:dump)
   end
 end
-  
+
+shared_examples 'file is invalid' do |file|
+  context file || 'nil' do
+    it 'returns "please provide correct file" message' do
+      expect { BitmapEditor.new.run(file) }.to output(/please provide correct file/).to_stdout
+    end
+  end
+end
+
+shared_examples 'file is valid' do |file, result, message = 'matches desired output'|
+  context file do
+    it message do
+      expect { BitmapEditor.new.run(file) }.to output(result).to_stdout
+    end
+  end
+end
--- spec/unit/lib/bitmap_editor_spec.rb
+++ spec/unit/lib/bitmap_editor_spec.rb
@@ -2,71 +2,19 @@ require 'spec_helper'
 
 describe BitmapEditor do
   describe '#run' do
-    context 'file is nil' do
-      it 'returns "provide correct file" message' do
-        expect { BitmapEditor.new.run(nil) }.to output(/please provide correct file/).to_stdout
-      end
-      context 'file does not exist' do
-        it 'returns "provide correct file" message' do
-          expect { BitmapEditor.new.run('examples/unit/does_not_exist.txt') }.to output(/please provide correct file/).to_stdout
-        end
-      end
-    end
+    it_behaves_like 'file is invalid', nil
+    it_behaves_like 'file is invalid', 'examples/unit/does_not_exist.txt'
+
     context 'read file' do
-      context 'Show bitmap' do
-        context 'image not created' do
-          it 'returns "There is no image" message' do
-            expect { BitmapEditor.new.run('examples/unit/show.txt') }.to output(/There is no image/).to_stdout
-          end
-        end
-      end
-      context 'Unknown command' do
-        it 'returns "unrecognised command" message' do
-          expect { BitmapEditor.new.run('examples/unit/unknown.txt') }.to output(/unrecognised command/).to_stdout
-        end
-      end
-      context 'Create bitmap image' do
-        it 'returns new bitmap' do
-          bitmap_image = "OOOOO\n" * 6
+      it_behaves_like 'file is valid', 'examples/unit/show.txt', /There is no image/, 'returns "There is no image" message'
+      it_behaves_like 'file is valid', 'examples/unit/unknown.txt', /unrecognised command/, 'returns "unrecognised command" message'
 
-          expect { BitmapEditor.new.run('examples/unit/create.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
-      context 'Colour pixel' do
-        it 'changes pixel colour at X Y' do
-          bitmap_image = "OOOOO\nOOOOO\nOOSOO\nOOOOO\nOOOOO\nOOOOO\n"
-
-          expect { BitmapEditor.new.run('examples/unit/colour.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
-      context 'Clear the image' do
-        it 'reset the bitmap image to default colour' do
-          bitmap_image = "OOOOO\n" * 6
-
-          expect { BitmapEditor.new.run('examples/unit/clear.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
-      context 'Draw a horizontal line' do
-        it 'changes pixel colour at X1 X2 Y' do
-          bitmap_image = "OZZZO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n"
-
-          expect { BitmapEditor.new.run('examples/unit/hline.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
-      context 'Draw a vertical line' do
-        it 'changes pixel colour at X Y1 Y2' do
-          bitmap_image = "OOOOO\nOOZOO\nOOZOO\nOOZOO\nOOZOO\nOOOOO\n"
-
-          expect { BitmapEditor.new.run('examples/unit/vline.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
-      context 'integration' do
-        it 'draws image' do
-          bitmap_image = "OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO\n"
-
-          expect { BitmapEditor.new.run('examples/integration/basic.txt') }.to output(bitmap_image).to_stdout
-        end
-      end
+      it_behaves_like 'file is valid', 'examples/unit/create.txt', "OOOOO\n" * 6, 'Creates an image'
+      it_behaves_like 'file is valid', 'examples/unit/colour.txt', "OOOOO\nOOOOO\nOOSOO\nOOOOO\nOOOOO\nOOOOO\n", 'Colours a pixel at X Y'
+      it_behaves_like 'file is valid', 'examples/unit/clear.txt', "OOOOO\n" * 6, 'Clears the bitmap'
+      it_behaves_like 'file is valid', 'examples/unit/hline.txt', "OZZZO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\nOOOOO\n", 'Draws a horizontal line'
+      it_behaves_like 'file is valid', 'examples/unit/vline.txt', "OOOOO\nOOZOO\nOOZOO\nOOZOO\nOOZOO\nOOOOO\n", 'Draws a vertical line'
+      it_behaves_like 'file is valid', 'examples/integration/basic.txt', "OOOOO\nOOZZZ\nAWOOO\nOWOOO\nOWOOO\nOWOOO\n", 'Basic integration test'
     end
   end
 end
