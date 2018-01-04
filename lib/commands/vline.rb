2871762b37a4e9b2d118060f4a1c5a9e4db0f01b
Chris Sullivan <email-blocked>
2018-01-04 22:39:05 +0000

    Tidy code, single call to cast / convert

--- lib/commands/vline.rb
+++ lib/commands/vline.rb
@@ -4,7 +4,10 @@ class VerticalLine
   attr_reader :range, :colour
   def initialize(column, start_row, end_row, colour)
     @colour = colour.to_s
-    @range = BitmapRange.new(column.to_i, start_row.to_i, column.to_i, end_row.to_i)
+    column = column.to_i
+    start_row = start_row.to_i
+    end_row = end_row.to_i
+    @range = BitmapRange.new(column, start_row, column, end_row)
   end
 
   def execute(bitmap = NilBitmap.new)
